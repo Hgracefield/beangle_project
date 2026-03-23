38c6aa4f05fda1cbb4241b57da28b4956bd277f8
tantan0gtlee <162418772+tantan0gtlee@users.noreply.github.com>
2026-03-23 17:02:48 +0900

    cors error handle
    cors error handle

--- Python/fastapi_route.py
+++ Python/fastapi_route.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from userDB import router as user_router
 import config
 from router.reservation import router as reservation_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user_router, prefix='/user', tags=['user'])  
 app.include_router(reservation_router,prefix='/reservation',tags=['reservations'])
 
+# CORS 설정
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['*'], # 모든 도메인을 허용
+  allow_credentials=True,
+  allow_methods=['*'], # 모든 http메서드 허용. 
+  allow_headers=['*'], # 모든 헤더 get,post... 
+)
 
 if __name__ == "__main__":
     import uvicorn
