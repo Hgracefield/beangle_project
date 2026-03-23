5561e93e6e0c673bcdeaa6856f6483d040ce2782
그필 <160110833+Hgracefield@users.noreply.github.com>
2026-03-23 16:37:27 +0900

    splash

---
+++ Python/config.py
@@ -0,0 +1,9 @@
+# config.py
+DB_HOST = "127.0.0.1"
+DB_PORT = 3307
+DB_USER = "root"
+DB_PASSWORD = "Qwer1234!"
+DB_NAME = "cycle-predict"
+DB_CHARSET = "utf8mb4"
+
+FASTAPI_HOST = "172.16.250.217"
--- beangle_app/lib/auth/auth_fastapi.py
+++ beangle_app/lib/auth/auth_fastapi.py
@@ -149,4 +149,4 @@ def update_user(user_id: int, req: AuthFastAPI):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="172.16.250.217", port=8000)
