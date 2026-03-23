b8184d82d1c08ea43f81a5b0b8f6832f5f3f9c29
tantan0gtlee <162418772+tantan0gtlee@users.noreply.github.com>
2026-03-23 14:35:38 +0900

    reservation api 추가
    reservation api 추가

--- Python/fastapi_route.py
+++ Python/fastapi_route.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from userDB import router as user_router
 import config
+from router.reservation import router as reservation_router
 
 app = FastAPI()
 app.include_router(user_router, prefix='/user', tags=['user'])  
+app.include_router(reservation_router,prefix='/reservation',tags=['reservations'])
 
 
 if __name__ == "__main__":
---
+++ Python/router/reservation.py
@@ -0,0 +1,128 @@
+from fastapi import APIRouter
+# from database.database import Dbconn
+from pydantic import BaseModel
+import pymysql
+import config
+
+router = APIRouter()
+	
+
+class ReservationModel(BaseModel):
+    reservation_id: int|None
+    user_id: int
+    station_id: int
+    time:str
+    is_cancel: int
+
+# def connect():
+#   conn = pymysql.connect(
+#     host='127.0.0.1',
+#     port=3307,
+#     user='root',
+#     password='Qwer1234!',
+#     database='cycle-predict',
+#     charset='utf8'
+#   )
+#   return conn
+def connect():
+    return pymysql.connect(
+        host=config.DB_HOST,
+        port=config.DB_PORT,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        database=config.DB_NAME,
+        charset=config.DB_CHARSET,
+        autocommit=False
+    )
+
+@router.get('/selectById/{id}')
+async def get(id: int):
+    conn = connect()
+    curs = conn.cursor()
+
+    sql = 'select * from reservation where reservation_id=%s' # (%s,%s,%s,%s)'
+    curs.execute(sql,[id])
+    rows = curs.fetchall()
+    
+    conn.close()
+    column_names = [desc[0] for desc in curs.description]
+    print(column_names)
+    #결과물 dictionary 형태로 전환
+    results = [dict(zip(column_names,row)) for row in rows]
+    print(results)
+
+    return {'results':results}
+
+
+
+@router.get('/select')
+async def get():
+    conn = connect()
+    curs = conn.cursor()
+
+    sql = 'select * from reservation order by reservation_id desc' # (%s,%s,%s,%s)'
+    curs.execute(sql,[])
+    rows = curs.fetchall()
+    
+    conn.close()
+    column_names = [desc[0] for desc in curs.description]
+    print(column_names)
+    #결과물 dictionary 형태로 전환
+    results = [dict(zip(column_names,row)) for row in rows]
+    print(results)
+
+    return {'results':results}
+
+
+@router.post('/insert')
+async def insert(data: ReservationModel):
+  
+  print('=== Insert Reservation ===')
+  try:
+    conn = connect()
+    curs = conn.cursor()
+
+    sql = 'INSERT INTO reservation(user_id, station_id, time, is_cancel) values(%s,%s,%s,%s)'
+    curs.execute(sql,[data.user_id, data.station_id, data.time, data.is_cancel])
+    conn.commit()
+    conn.close()
+
+    return {'result':1}
+  except Exception as err:
+    print('ERROR(INSERT): ')
+    print(err)
+    return {'result':0}
+
+@router.post('/update')
+async def update(data: ReservationModel):
+    try:
+        print('=== update Reservation ===')
+        conn = connect()
+        curs = conn.cursor()
+
+        sql = 'update reservation set station_id=%s, time=%s, is_cancel=%s where reservation_id=%s'
+        curs.execute(sql,[data.station_id, data.time, data.is_cancel,data.reservation_id])
+        conn.commit()
+        conn.close()
+        return {'result':1}
+    except Exception as err:
+       print('=== ERROR(UPDATE): ')
+       print(err)
+       return {'result':0}
+
+@router.post('/delete')
+async def delete(data: ReservationModel):
+    try:
+        print('=== delete Reservation ===')
+        conn = connect()
+        curs = conn.cursor()
+
+        sql = 'delete from reservation where reservation_id=%s and user_id=%s'
+        curs.execute(sql,[data.reservation_id,data.user_id])
+        conn.commit()
+        conn.close()
+        return {'result':1}
+    except Exception as err:
+       print('=== ERROR(DELETE): ')
+       print(err)
+       return {'result':0}
