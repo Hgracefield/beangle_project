88d491a97415131d709725b6925f8604c340f34a
Restitutor <restitutor1008@gmail.com>
2026-03-23 10:44:07 +0900

    23-Mar-2026 Chansol Park desc:Syncronized dashboard.dart

--- beangle_app/lib/main.dart
+++ beangle_app/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:beangle_app/app_shell.dart';
 import 'package:beangle_app/auth/auth_find_info.dart';
 import 'package:beangle_app/auth/auth_page.dart';
 import 'package:beangle_app/auth/auth_sign_page.dart';
-import 'package:beangle_app/dashboard/dashboard.dart';
 import 'package:beangle_app/reservation/reservation.dart';
 import 'package:beangle_app/user/map_for_user.dart';
 import 'package:beangle_app/worker/view/map_for_worker.dart';
